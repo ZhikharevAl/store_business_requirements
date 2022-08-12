58ed0f15a4bdef61be6b1d4036126f85deae1f9b
AlexeyZh <waltafunk@gmail.com>
2022-08-12 16:21:53 +0300

    add generated person

---
+++ generator/generator.py
@@ -0,0 +1,19 @@
+import random
+
+from data.data import Person
+
+from faker import Faker
+
+faker_ru = Faker('ru_RU')
+fake_en = Faker('En')
+Faker.seed()
+
+
+def generated_person():
+    yield Person(
+        full_name=faker_ru.first_name() + " " + faker_ru.last_name() + " " + faker_ru.middle_name(),
+        firstname=faker_ru.first_name(),
+        lastname=faker_ru.last_name(),
+        email=faker_ru.email(),
+        mobile=faker_ru.msisdn(),
+    )
